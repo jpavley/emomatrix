0d756d401e996427eebac055b09e7f554a21b7a2
John Pavley <john@pavley.com>
2021-08-12 17:43:56 -0400

    using offsetX and Y

--- index.js
+++ index.js
@@ -12,15 +12,42 @@ canvas.style.backgroundColor = 'black';
 // locations
 let centerX = canvas.width / 2;
 let centerY = canvas.height / 2;
+
+// mouse click
+
 let clickX = centerX;
 let clickY = centerY;
 
 canvas.addEventListener('click', (e) => {
-
-    const rect = canvas.getBoundingClientRect();
-    clickX = e.clientX - rect.left;
-    clickY = e.clientY - rect.top;
+    clickX = e.offsetX;
+    clickY = e.offsetY;
 });
+
+// mouse move
+
+let mouseX = 0;
+let mouseY = 0;
+
+canvas.addEventListener('mousemove', (e) => {
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+});
+
+function drawMouseMove(ctx) {
+    ctx.fillStyle = 'yellow';
+    ctx.font = '14px monospace';
+    ctx.fillText(`${mouseX},${mouseY}`, 35, 575);
+}
+
+let frameRate = 0;
+let previousFrameTime = 0;
+let timeStamp = performance.now();
+
+function drawFrameRate(ctx) {
+    ctx.fillStyle = 'yellow';
+    ctx.font = '14px monospace';
+    ctx.fillText(`${frameRate}`, 840, 575);
+}
 
 document.body.prepend(canvas);
 const ctx = canvas.getContext('2d');
@@ -60,6 +87,9 @@ function draw() {
     const textMetrics = ctx.measureText(testText);
 
     ctx.fillText(testText, clickX, clickY);
+
+    drawMouseMove(ctx);
+    drawFrameRate(ctx);
 
     game.requestID = requestAnimationFrame(draw);
 }

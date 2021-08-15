287fbcc2d46981e7ed89f9210c3d25dab8c89dad
John Pavley <john@pavley.com>
2021-08-15 13:25:39 -0400

    complete list of green or mostly green emoji

--- emoji.js
+++ emoji.js
@@ -1,4 +1,109 @@
 /**
  * Emoji.js
  * A library of emoji data and functions.
- */ 
+ */
+
+export const fullEmojiList = "https://unicode.org/emoji/charts/full-emoji-list.html#1f9d1_200d_1f3a4";
+
+export const greenEmojiCodepoints = [
+    // Fruit and Veggies
+    0x1F34F, // Green Apple
+    0x1F966, // Broccoli
+    0x1F952, // Cucumber
+    0x1F957, // Green Salad
+    0x1F951, // Avocado
+    0x1F95D, // Kiwi
+    0x1F96C, // Leafy Green
+    0x1F348, // Melon
+    0x1F350, // Pear
+    0x1F33D, // Ear of Corn
+    0x1FAD2, // Olive
+    0x1FAD1, // Bell Pepper
+    // Drinks
+    0x1F37E, // Bottle with Popping Cork
+    0x1F378, // Cocktail Glass
+    0x1F9C3, // Beverage Box
+    // Symbols
+    0x1F49A, // Green Heart
+    0x1F384, // Chritmas Tree
+    0x1F38B, // Tanabata Tree
+    0x1F67B, // Recycling Symbol
+    0x1F530, // Japanese Beginner
+    0x2705, // Check Mark Button
+    0x274E, // Cross Mark Button
+    0x2733, // Asterisk Button
+    0x2747, // Sparkle Button
+    0x1F22F, // Japanese Reserved Button
+    0x1F7E2, // Green Circle
+    0x1F7E9, // Green Square
+
+    // Smilies
+    0x1F92E, // Face Vomiting
+    0x1F922, // Nauseated Face
+    0x1F911, // Money Mouth Face
+    // Things
+    0x1F4D7, // Green Book
+    0x1F52B, // Water Pistol
+    0x1FA9B, // Screwdriver
+    0x1F9EA, // Test Tube
+    0x1F9FC, // Soap
+    // People
+    0x1F9D1, // Singer
+    0x1F9DA, // Fairy
+    0x1F9DF, // Zombie
+    // Animals
+    0x1F40E, // Horse
+    0x1F99A, // Peacock
+    0x1F99C, // Parrot
+    0x1F438, // Frog
+    0x1F40A, // Crocodile
+    0x1F422, // Turtle
+    0x1F98E, // Lizard
+    0x1F40D, // Snake
+    0x1F409, // Dragon
+    0x1F996, // T-Rex
+    // Bugs
+    0x1F41B, // Bug
+    0x1FAB2, // Beetle
+    0x1F9A0, // Microbe
+    // Plants
+    0x1F331, // Seedling
+    0x1FAB4, // Potted Plan
+    0x1F332, // Evergreen Tree
+    0x1F333, // Deciduous Tree
+    0x1F334, // Palm Tree
+    0x1F335, // Cactus
+    0x1F33E, // Sheaf of Rice
+    0x1F33F, // Herb
+    0x2618, // Shamrock
+    0x1F340, // Four Leaf Clover
+    0x1F343, // Leaf Fluttering
+    // Buildings
+    0x1F5FD, // Statue of Liberty
+    // Automobiles
+    0x1F69B, // Articulated Truck
+    // Boats
+    0x1F6F6, // Canoe
+    // Sports
+    0x1F94E, // Softball
+    0x1F3BE, // Tennis
+    0x1F6F3, // Flag in Hole
+    // Games
+    0x1FA80, // yo-yo
+    0x1F9E9, // Puzzle Piece
+    // Clothing
+    0x1F9E4, // Glove
+    0x1FA72, // Briefs
+    0x1FA74, // Thong  Sandal
+    0x1F452, // Woman's Hat
+    0x1FA96, // Military Helmet
+    // Computer and Eletronics
+    0x1F4D4, // Pager
+    0x1F50B, // Battery
+    // Money
+    0x1F4B4, // Yen Banknote
+    0x1F4B5, // Dollar Banknote
+    0x1F4B6, // Euro Banknote
+    0x1F4B8, // Money with Wings
+
+];
